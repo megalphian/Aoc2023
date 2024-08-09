2f3f8311a6ad41f6a5bda8686f53055d26e51321
Megnath Ramesh <megnath.ramesh@gmail.com>
2024-08-09 09:04:23 -0400

    Day 3 changes

--- day3/solve.py
+++ day3/solve.py
@@ -1,9 +1,14 @@
 engine = 'input.txt'
 with open(engine) as f:
     lines = f.readlines()
-
+vals = []
+row_nums = []
+col_nums = []
 number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 dot = '.'
 for row in range(len(lines)):
+    current_num = None
     for col in range(len(lines[row])):
-        pass
+       if lines[row][col] in number:
+           num_char = lines[row][col]
+           pass
