587452e6aadab1b1f0cda19286a2051fed243171
Michelle Hannah <michellechristineh@gmail.com>
2024-08-28 13:42:53 -0400

    Day4 Working Pt2

--- day4/solve.py
+++ day4/solve.py
@@ -4,14 +4,20 @@ with open(input_filename) as f:
     lines = f.readlines()
 
 winnings = 0
-number = ['0','1','2','3','4','5','6','7','8','9']
+multiplier_list = []
 
-for line in lines:
+for id, line in enumerate(lines):
     num_str = line.strip('\n').split(':')[1]
     win_str, have_str = num_str.split('|')
 
-    count = len(set(win_str.split()) & set(have_str.split()))
-    if count > 0:
-        winnings += 2 ** (count - 1)
+    multiplier_list.append(id)
+    multiplier = multiplier_list.count(id)
+
+    matches = len(set(win_str.split()) & set(have_str.split()))
+
+    for rep in range(multiplier):
+        for match in range(matches):
+            multiplier_list.append(id+1+match)
+    winnings = winnings + multiplier
 
 print(winnings)
