393506830c9c86f2b185c7290a3f895344b51688
Michelle Hannah <michellechristineh@gmail.com>
2024-08-27 20:17:28 -0400

    Day 3 cleaned up

--- day3/solve.py
+++ day3/solve.py
@@ -7,41 +7,29 @@ num_coor = []
 confirm_list = []
 
 number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-dot = '.'
 gear = '*'
 
-current_num = None
+current_num = ''
 num_coor = []
 
 for row in range(len(lines)):
-    if current_num is not None:
+    if len(current_num) > 0:
         vals.append([current_num, num_coor])
-        current_num = None
+        current_num = ''
         num_coor = []
     for col in range(len(lines[row])-1):  
         char = lines[row][col]
         if char in number:
-            if current_num is None:
-                current_num = char
-            elif current_num is not None:
-                current_num = current_num + char
+            current_num = current_num + char
             num_coor.append([row,col])
-        elif char == dot:
-            if current_num is not None:
-                vals.append([current_num, num_coor])
-                current_num = None
-                num_coor = []
-        elif char == gear:
-            if current_num is not None:
-                vals.append([current_num, num_coor])
-                current_num = None
-                num_coor = []
-            gear_list.append([row,col])
         else:
-            if current_num is not None:
+            if len(current_num) > 0:
                 vals.append([current_num, num_coor])
-                current_num = None
+                current_num = ''
                 num_coor = []
+
+            if char == gear:
+                gear_list.append([row,col])
 
 for gear_id in range(len(gear_list)):
     for val, coords in vals:
@@ -49,17 +37,9 @@ for gear_id in range(len(gear_list)):
         y_id = 1
         is_adj = False
         for num_id in range(len(coords)):
-            if coords[num_id][x_id] == gear_list[gear_id][x_id]:
-                if abs(coords[num_id][y_id] - gear_list[gear_id][y_id]) == 1:
-                    is_adj = True
-                    confirm_list.append([gear_list[gear_id], val])
-            elif abs(coords[num_id][x_id] - gear_list[gear_id][x_id]) == 1:
-                if coords[num_id][y_id] == gear_list[gear_id][y_id]:
-                    is_adj = True
-                    confirm_list.append([gear_list[gear_id], val])
-                elif abs(coords[num_id][y_id] - gear_list[gear_id][y_id]) == 1:
-                    is_adj = True
-                    confirm_list.append([gear_list[gear_id], val])
+            if (abs(coords[num_id][x_id] - gear_list[gear_id][x_id]) <= 1 and abs(coords[num_id][y_id] - gear_list[gear_id][y_id]) <= 1):
+                is_adj = True
+                confirm_list.append([gear_list[gear_id], val])
             if is_adj:
                 break
 product_list = []
@@ -70,6 +50,3 @@ for h in range(len(confirm_list)):
         product_list.append(int(confirm_list[h][1])*int(confirm_list[h-1][1]))
 
 print(sum(product_list))
-
-
-    
