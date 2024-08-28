760c028ed9bf2f00834f5ec67013ed7d29564c33
Michelle Hannah <michellechristineh@gmail.com>
2024-08-28 14:02:56 -0400

    day4 cleanedup

--- day4/solve.py
+++ day4/solve.py
@@ -3,21 +3,15 @@ input_filename = 'day4/input.txt'
 with open(input_filename) as f:
     lines = f.readlines()
 
-winnings = 0
-multiplier_list = []
+cards = [1] * len(lines)
 
 for id, line in enumerate(lines):
     num_str = line.strip('\n').split(':')[1]
     win_str, have_str = num_str.split('|')
 
-    multiplier_list.append(id)
-    multiplier = multiplier_list.count(id)
-
     matches = len(set(win_str.split()) & set(have_str.split()))
 
-    for rep in range(multiplier):
-        for match in range(matches):
-            multiplier_list.append(id+1+match)
-    winnings = winnings + multiplier
+    for match in range(matches):
+        cards[match+id+1] += cards[id]
 
-print(winnings)
+print(sum(cards))
