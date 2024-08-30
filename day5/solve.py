25e37b0762d13f4bcefaeec9039fcb31bf1bd57f
Michelle Hannah <michellechristineh@gmail.com>
2024-08-30 11:16:07 -0400

    New version for day 5 part 2

--- day5/solve.py
+++ day5/solve.py
@@ -29,25 +29,97 @@ for line in lines:
             sections[sec_num] += [line]
 
 seed_list = sections[0][0]
+seed_ranges = []
+count = 0
+seeds = []
+
+for i in range(len(seed_list)):
+    if i % 2 == 0:
+        start = int(seed_list[i])
+        length = int(seed_list[i+1])
+        end = start + length
+        seed_ranges += [[start, end]]
+        count += length
+
+# print(seed_range)
+# print(count)
+
+# Handle overlap between seed ranges
+
+# Sort the seed ranges by the start value
+seed_ranges = sorted(seed_ranges, key=lambda seed_range: seed_range[0])
+
+# Handle seed range overlap
+for i in range(len(seed_ranges)-1):
+    for j in range(i+1, len(seed_ranges)):
+        if(len(seed_ranges[i]) == 0 or len(seed_ranges[j]) == 0):
+            continue
+        a_1, b_1 = seed_ranges[i]
+        a_2, b_2 = seed_ranges[j]
+        if(a_2 > b_1):
+            continue
+        elif(b_1 >= a_2 and b_2 > b_1):
+            seed_ranges[i] = [a_1, b_2]
+            seed_ranges[j] = []
+        elif(b_1 >= a_2 and b_2 <= b_1):
+            seed_ranges[i] = [a_1, b_1]
+            seed_ranges[j] = []
+
 location = None
+ref_ranges = seed_ranges.copy()
 
-for seed in seed_list:
-    ref_number = int(seed)
+loop_length = len(sections[1:])
+counter = 0
 
-    for id, section in enumerate(sections[1:]):
-        for dest, source, len in section:
-            min = int(source)
-            max = int(source) + int(len)
-            shift = int(dest) - int(source)
+for section in sections[1:]:
+    new_ref_ranges = []
+    shifts = []
+    split_pairs = []
+    for dest, source, length in section:
+        # add split pairs to ref ranges and clear split pairs
+        ref_ranges += split_pairs
+        split_pairs = []
 
-            if ref_number >= min and ref_number <= max:
-                ref_number += shift
-                break   
+        min = int(source)
+        max = int(source) + int(length)
+        shift = int(dest) - int(source)
 
+        for ref_range in ref_ranges:
+            a, b = ref_range
+            # There is overlap between the map and the ranges we have
+            if(min >= a and min <= b):
+                if(max <= b):
+                    transform_pair = [min, max]
+                    new_split_pairs = [[a, min-1], [max+1, b]]
+                else:
+                    transform_pair = [min, b]
+                    new_split_pairs = [[a, min-1]]
+            elif(max >= a and max <= b):
+                # recall min is not within the range
+                transform_pair = [a, max]
+                new_split_pairs = [[max + 1, b]]    
+            
+            new_ref_ranges.append(transform_pair)
+            shifts.append(shift)
+
+            new_split_pairs = [split_pair for split_pair in split_pairs if split_pair[0] <= split_pair[1]]
+            split_pairs += new_split_pairs
+    ref_ranges += split_pairs
+    split_pairs = []
+
+    for i in range(len(new_ref_ranges)):
+        new_range = new_ref_ranges[i]
+        shift = shifts[i]
+        ref_ranges += [[a + shift for a in new_range]]
+
+    counter += 1
+    print('Done section '+ str(counter) + ' out of ' + str(loop_length))
+
+for a, b in ref_ranges:
     if location is None:
-        location = ref_number
-    elif location > ref_number:
-        location = ref_number            
+        location = a
+    elif location > a:
+        location = a
 
 print(location)
         
